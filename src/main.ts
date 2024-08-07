7b79fb53135d28154709be6a6476223caf275845
amdamv <madakhmedov@gmail.com>
2024-08-06 20:13:49 -0400

    changing names

--- src/main.ts
+++ src/main.ts
@@ -10,10 +10,10 @@ async function bootstrap() {
   app.useGlobalFilters(new HttpExceptionFilter());
   app.enableCors();
   const config = new DocumentBuilder()
-      .setTitle('Cats example')
-      .setDescription('The cats API description')
+      .setTitle('Users Example')
+      .setDescription('The users API description')
       .setVersion('1.0')
-      .addTag('cats')
+      .addTag('users')
       .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
