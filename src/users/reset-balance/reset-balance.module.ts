0cb60d7067585e1354fd59d3981d9b827a628fb3
amdamv <madakhmedov@gmail.com>
2024-09-04 16:36:47 -0400

    (job)

--- src/users/reset-balance/reset-balance.module.ts
+++ src/users/reset-balance/reset-balance.module.ts
@@ -1,10 +1,12 @@
 import { Module } from "@nestjs/common";
 import { ResetBalanceController } from "./reset-balance.controller";
+import { ResetBalanceProcessor } from "./reset-balance.processor";
 import { ResetBalanceService } from "./reset-balance.service";
 import { BullModule } from "@nestjs/bull";
-import { ResetBalanceProcessor } from "./reset-balance.processor";
 import { UsersModule } from "../users.module";
 import { RESET_BALANCE_QUEUE } from "../../common/constants/name-queue.constant";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { UserEntity } from "../entity/user.entity";
 
 @Module({
   imports: [
@@ -12,9 +14,10 @@ import { RESET_BALANCE_QUEUE } from "../../common/constants/name-queue.constant"
     BullModule.registerQueue({
       name: RESET_BALANCE_QUEUE,
     }),
+    TypeOrmModule.forFeature([UserEntity]),
   ],
   controllers: [ResetBalanceController],
-  providers: [ResetBalanceService, ResetBalanceProcessor],
+  providers: [ResetBalanceProcessor, ResetBalanceService],
   exports: [ResetBalanceService],
 })
 export class ResetBalanceModule {}
--- src/users/reset-balance/reset-balance.processor.ts
+++ src/users/reset-balance/reset-balance.processor.ts
@@ -1,30 +1,27 @@
 import { Process, Processor } from "@nestjs/bull";
 import { Job } from "bull";
-import { UsersService } from "../users.service";
 import { Logger } from "@nestjs/common";
 import { RESET_BALANCE_QUEUE } from "../../common/constants/name-queue.constant";
+import { InjectRepository } from "@nestjs/typeorm";
+import { UserEntity } from "../entity/user.entity";
+import { Repository } from "typeorm";
 
 @Processor(RESET_BALANCE_QUEUE)
 export class ResetBalanceProcessor {
   private logger = new Logger("ResetBalanceProcessor");
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
 
   @Process("add-reset-balances")
-  async handleResetBalances(job: Job) {
-    try {
-      // Logic to reset all user balances
-      this.logger.log(`Processing job ${job.id} to reset balances`);
-      const users = await this.usersService.findAll();
-
-      this.logger.log(`Resetting balances for ${users.length} users`);
-      for (const user of users) {
-        user.balance = 0;
-        await this.usersService.updateUserBalance(user.id, user.balance);
-      }
-      this.logger.log(`Job ${job.id} completed successfully`);
-    } catch (err) {
-      this.logger.error("Error resetting balances:", err);
-      await job.moveToFailed({ message: err.message }, true);
-    }
+  async resetBalance(job: Job) {
+    this.logger.log(`Processing reset balance job with id: ${job.id}`);
+    await this.userRepository
+      .createQueryBuilder()
+      .update(UserEntity)
+      .set({ balance: 0 })
+      .execute();
+    this.logger.log(`Reset balance job has been processed with id: ${job.id}`);
   }
 }
